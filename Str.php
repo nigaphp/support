d0a1e4d64ef8c417844866d65d6c034552dddfef
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-02 15:43:59 +0000

    First commit

---
+++ Str.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the Nigatedev PHP framework package
+ *
+ * (c) Abass Ben Cheik <abass@todaysdev.com>
+ */
+
+namespace Nigatedev\Framework\Support;
+
+use Nigatedev\Framework\Support\Exception\StrException;
+
+/**
+ * String supports
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+class Str extends AbstractStr
+{
+  
+   /**
+    * @{inheritdoc}
+    */
+    public static function len($str)
+    {
+        return self::strLength($str);
+    }
+  
+   /**
+    * @{inheritdoc}
+    */
+    public static function pos($str, $pos)
+    {
+        return self::strPos($str, $pos);
+    }
+  
+   /**
+    * @{inheritdoc}
+    */
+    public static function isNum($isNum)
+    {
+        return self::isNumeric($isNum);
+    }
+  
+   /**
+    * @{inheritdoc}
+    */
+    public static function isInt($isInt)
+    {
+        return self::isInteger($isInt);
+    }
+          
+}
